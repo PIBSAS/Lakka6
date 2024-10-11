70f7a7bba06bf6f59eac69ca52fcb7b0e2dcaa6a
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2024-10-11 00:09:22 -0300

    Update lakka.sh

--- lakka.sh
+++ lakka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###########################################################################
 # Repositorio: Lakka 6 2024
-# Por: Raspberry Pi Buenos Aires ("https://sites.google.com/view/raspberrypibuenosaires/)
+# Por: Luciano's tech ("https://sites.google.com/view/lucianostech/)
 # License: http://creativecommons.org/licenses/by-sa/4.0/
 ###########################################################################
 RUTA_ARCADE="https://github.com/PIBSAS/Lakka6/raw/main/System/Arcade/"
